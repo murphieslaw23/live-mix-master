11605959f0012076adf63e12530ea8881eefcc3d
Murphies Law <emilach82@gmail.com>
2026-09-12 12:01:16 +0200

    feat(web): route Vercel to reference composition

--- lib/app/app_surface.dart
+++ lib/app/app_surface.dart
@@ -1,3 +1,3 @@
 export 'app_surface_stub.dart'
     if (dart.library.io) 'app_surface_desktop.dart'
-    if (dart.library.js_interop) 'app_surface_web.dart';
+    if (dart.library.js_interop) 'app_surface_web_reference.dart';
